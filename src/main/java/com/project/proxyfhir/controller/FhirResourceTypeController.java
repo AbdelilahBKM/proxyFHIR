24916db4662698304942844778b12894e82665dc
AbdelilahBKM <boukhatem.abdelilah@outlook.com>
2025-11-22 11:12:19 +0100

    update 11/22

--- src/main/java/com/project/proxyfhir/controller/FhirResourceTypeController.java
+++ src/main/java/com/project/proxyfhir/controller/FhirResourceTypeController.java
@@ -3,7 +3,6 @@ package com.project.proxyfhir.controller;
 import com.project.proxyfhir.model.FhirResource;
 import com.project.proxyfhir.repository.FhirResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
